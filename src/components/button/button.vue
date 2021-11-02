cda2c7dd197ae7f023b751ff9c88fe87248c76b0
Thomas Gaubet <thomas.gaubert49@gmail.com>
2021-11-02 17:02:31 +0100

    feat: scss and first component

---
+++ src/components/button/button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="dt-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'dt-button',
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="dt-p16">
+    <dt-button>bonjour</dt-button>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue';
+import button from '../components/button/button.vue';
 
-@Options({
+export default defineComponent({
   components: {
-    HelloWorld,
+    'dt-button': button,
   },
-})
-export default class Home extends Vue {}
+});
 </script>
