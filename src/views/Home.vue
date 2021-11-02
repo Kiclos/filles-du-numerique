60b4cb36b46ff94b3f0da6c01ebf4ce8efb00543
alegrand <alan.le-grand@protonmail.com>
2021-11-02 23:23:20 +0100

    fix : adding blue color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="dt-p16">
+  <div class="dt-p16" style="background: #f6f6f6">
     <Button class="dt-button--default">Commencer le jeu</Button>
+    <Button class="dt-button--text">Commencer le jeu</Button>
+    <Button class="dt-button--reversed-text">Commencer le jeu</Button>
+    <Button class="dt-button--outlined">Commencer le jeu</Button>
+    <Button class="dt-button--reversed-outlined">Commencer le jeu</Button>
+    <Button class="dt-button--reversed">Commencer le jeu</Button>
+    <Button class="dt-button--blue">Commencer le jeu</Button>
   </div>
 </template>
 
