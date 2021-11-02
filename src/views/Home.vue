49a29aa4104313265aa7b1766d15ac1fa6c7a1fc
alegrand <alan.le-grand@protonmail.com>
2021-11-02 23:18:33 +0100

    feat : adding text containers with colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="dt-p16">
     <Button class="dt-button">bonjour</Button>
+    <TextContainer class="dt-textcontainer--default">
+      <p>Quoi qu’il est peut être juste éteint...</p>
+    </TextContainer>
+    <TextContainer class="dt-textcontainer--blue">
+      <p>Quoi qu’il est peut être juste éteint...</p>
+    </TextContainer>
+    <TextContainer class="dt-textcontainer--neutral">
+      <p>Quoi qu’il est peut être juste éteint...</p>
+    </TextContainer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Button from '../components/Button/Button.vue';
+import TextContainer from '@/components/TextContainer/TextContainer.vue';
 
 export default defineComponent({
   components: {
+    TextContainer,
     Button,
   },
 });
