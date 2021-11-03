3d7e314cba1db935f1708caff1c4dad51cf4c445
alegrand <alan.le-grand@protonmail.com>
2021-11-03 08:21:22 +0100

    feat : adding TapToContinue component

---
+++ src/components/TapToContinue/TapToContinue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="dt-flex dt-flex-justify--end dt-taptocontinue">
+    <p class="dt-taptocontinue-text">Taper pour continuer...</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TapToContinue',
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="dt-p16">
     <Button class="dt-button">bonjour</Button>
+    <TapToContinue/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Button from '../components/Button/Button.vue';
+import TapToContinue from '@/components/TapToContinue/TapToContinue.vue';
 
 export default defineComponent({
   components: {
+    TapToContinue,
     Button,
   },
 });
