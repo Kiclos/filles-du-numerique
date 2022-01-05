1a69bc0cc61fbaf6fa0eace42b4e694492ccf31b
ClementineBiais <clementine.biais@live.fr>
2022-01-05 12:10:12 +0100

    update results and welcome views

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,23 @@
   <transition name="zoom-fade">
     <DialogView :dialogs="dialogs" @close="dialogIsOpened = false" v-if="dialogIsOpened"/>
   </transition>
+  <transition name="zoom-fade">
+    <WelcomeView @play="welcomeIsOpened = false" v-if="welcomeIsOpened"/>
+  </transition>
+  <transition name="zoom-fade">
+    <ResultsView name="dialogs" @close="resultIsOpened = false" v-if="resultIsOpened"/>
+  </transition>
   <Button class="dt-button--default" @click="dialogIsOpened = true">Open dialog</Button>
+  <Button class="dt-button--default" @click="welcomeIsOpened = true">Open welcome</Button>
+  <Button class="dt-button--default" @click="resultIsOpened = true">Open results</Button>
   <Home/>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from 'vue';
 import DialogView from '@/views/Dialog/Dialog.vue';
+import WelcomeView from '@/views/Welcome.vue';
+import ResultsView from '@/views/Results.vue';
 import Home from '@/views/Home.vue';
 import Button from '@/components/Button/Button.vue';
 import Dialog from '@/Model/Dialog';
@@ -17,11 +27,15 @@ export default defineComponent({
   name: 'app',
   components: {
     DialogView,
+    WelcomeView,
+    ResultsView,
     Home,
     Button,
   },
   setup() {
     const dialogIsOpened = ref<boolean>(false);
+    const welcomeIsOpened = ref<boolean>(true);
+    const resultIsOpened = ref<boolean>(false);
     const dialogs = reactive<Dialog []>([
       { id: 0, isRebecca: true, content: 'Bonjour' },
       { id: 1, isRebecca: false, content: 'Bonjour à toi aussi' },
@@ -29,6 +43,8 @@ export default defineComponent({
 
     return {
       dialogIsOpened,
+      welcomeIsOpened,
+      resultIsOpened,
       dialogs,
     };
   },
