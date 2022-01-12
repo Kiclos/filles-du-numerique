031be81d05d309d8576d855b2a3d67e9569a40fd
ClementineBiais <clementine.biais@live.fr>
2022-01-12 15:01:15 +0100

    add results & welcome views

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
   <transition name="zoom-fade">
     <DialogView :dialogs="dialogs" @close="dialogIsOpened = false" v-if="dialogIsOpened"/>
   </transition>
+  <transition name="zoom-fade">
+    <WelcomeView @play="welcomeIsOpened = false" v-if="welcomeIsOpened"/>
+  </transition>
+  <transition name="zoom-fade">
+    <ResultsView name="dialogs" @close="resultIsOpened = false" v-if="resultIsOpened"/>
+  </transition>
   <Button class="dt-button--default" @click="dialogIsOpened = true">Open dialog</Button>
-  <IslandsView/>
+  <Button class="dt-button--default" @click="welcomeIsOpened = true">Open welcome</Button>
+  <Button class="dt-button--default" @click="resultIsOpened = true">Open results</Button>
+  <IslandsView v-if="false"/>
   <Home v-if="false"/>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from 'vue';
 import DialogView from '@/views/Dialog.vue';
+import WelcomeView from '@/views/Welcome.vue';
+import ResultsView from '@/views/Results.vue';
 import Home from '@/views/Home.vue';
 import Button from '@/components/Button/Button.vue';
 import Dialog from '@/Model/Dialog';
@@ -19,12 +29,17 @@ export default defineComponent({
   name: 'app',
   components: {
     DialogView,
+    WelcomeView,
+    ResultsView,
     Home,
     Button,
     IslandsView,
   },
+
   setup() {
     const dialogIsOpened = ref<boolean>(false);
+    const welcomeIsOpened = ref<boolean>(true);
+    const resultIsOpened = ref<boolean>(false);
     const dialogs = reactive<Dialog []>([
       { id: 0, isRebecca: true, content: 'Bonjour' },
       { id: 1, isRebecca: false, content: 'Bonjour à toi aussi' },
@@ -32,6 +47,8 @@ export default defineComponent({
 
     return {
       dialogIsOpened,
+      welcomeIsOpened,
+      resultIsOpened,
       dialogs,
     };
   },
---
+++ src/views/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dt-p16-center dt-h100">
+    <img class="dt-img-w75" src="@/assets/departile.svg" alt="">
+    <h1 class="dt-h1">DigitTales</h1>
+    <br>
+    <Button class="dt-button--default" @click="$emit('play')">Jouer</Button>
+    <Button class="dt-button--outlined">Crédits</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from '../components/Button/Button.vue';
+
+export default defineComponent({
+  name: 'welcome',
+  components: {
+    Button,
+  },
+});
+</script>
