265386fd7c1e39ffb7feff9e87765f61b60ddfdd
ClementineBiais <clementine.biais@live.fr>
2022-01-12 15:03:11 +0100

    add presentation screen

---
+++ src/views/Presentation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="background: linear-gradient(#0093E9, #80D0C7);" class="dt-p16">
+    <button>back</button>
+    <button>passer le mini jeu</button>
+    <img class="dt-p12" src="@/assets/circle.png" alt="">
+    <h2>Île Robotix</h2>
+    <p>Sur cette île... Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Soluta, explicabo. Beatae labore ducimus corporis autem atque itaque dolor in quidooem
+      veniam, voluptatum libero unde saepe delectus fugiat enim consectetur expedita!</p>
+    <Button class="dt-button--blue">Commencer</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from '../components/Button/Button.vue';
+
+export default defineComponent({
+  name: 'game-results',
+  components: {
+    Button,
+  },
+});
+</script>
