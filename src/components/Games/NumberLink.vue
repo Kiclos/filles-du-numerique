8a8aeafb3ff8809e35a9a8b89d3824436b5fc15f
Thomas Gaubet <thomas.gaubert49@gmail.com>
2022-01-24 12:29:10 +0100

    feat: random generation of paths

--- src/components/Games/NumberLink.vue
+++ src/components/Games/NumberLink.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="dt-number-link">
-    <div class="dt-number-link__row" v-for="(row, i) in game.rows" :key="'row' + i">
-      <div v-for="(c, j) in row.cases" :key="'case' + j"
-          class="dt-number-link__case -hoverable"
-           :class="getClass(c)"
-          @click="handleSelect(c)">
+  <div class="dt-number-link__content">
+    <div class="dt-number-link">
+      <div class="dt-number-link__row" v-for="(row, i) in game.rows" :key="'row' + i">
+        <div v-for="(c, j) in row.cases" :key="'case' + j"
+             class="dt-number-link__case -hoverable"
+             :class="c.classes"
+             @click="handleSelect(c)">
+        </div>
       </div>
     </div>
   </div>
@@ -15,10 +17,22 @@ import { defineComponent, onMounted, reactive } from 'vue';
 
 export default defineComponent({
   name: 'NumberLink',
-  setup() {
+  events: { endGame: () => null },
+  setup(_, { emit }) {
+    // eslint-disable-next-line no-shadow
+    enum PathColor {
+      black = 'black',
+      blue = 'blue',
+      green = 'green',
+      purple= 'purple',
+      red = 'red',
+      yellow = 'yellow'
+    }
+
     interface Case {
       x: number,
       y: number,
+      classes: string [],
     }
     interface Row {
       cases: Case []
@@ -27,13 +41,13 @@ export default defineComponent({
       rows: Row []
     }
     interface Path {
-      color: string,
+      color: PathColor,
       cases: Case [],
       goal: Case,
     }
 
     const game: Grid = reactive<Grid>({ rows: [] });
-    const paths: Path [] = reactive<Path []>([]);
+    let paths: Path [] = reactive<Path []>([]);
 
     function getNeighbors(c: Case): Case [] {
       const res: Case [] = [];
@@ -57,34 +71,11 @@ export default defineComponent({
     }
 
     function isGameOver(): boolean {
-      return paths.every((p) => isNeighbors(p.goal, p.cases[p.cases.length - 1]));
+      return paths.every((p) => isNeighbors(p.goal, p.cases[p.cases.length - 1]) && p.cases.length > 1);
     }
 
     function getCasePath(c: Case): Path | undefined {
       return paths.find((p) => p.cases.indexOf(c) !== -1);
-    }
-
-    function handleSelect(c: Case): void {
-      const neighbors: Case [] = getNeighbors(c);
-      const casePath: Path | undefined = getCasePath(c);
-      if (casePath && casePath.cases[casePath.cases.length - 1] === c && casePath.cases[0] !== c) {
-        casePath.cases.splice(casePath.cases.length - 1, 1);
-      } else if (!casePath) {
-        const pathNeighbors: Case [] = neighbors.filter((n) => {
-          const path = getCasePath(n);
-          if (path) {
-            return path.cases[path.cases.length - 1] === n;
-          }
-          return false;
-        });
-        if (pathNeighbors.length > 0) {
-          const path: Path | undefined = getCasePath(pathNeighbors[0]);
-          if (path) {
-            path.cases.push(c);
-          }
-        }
-      }
-      console.log('is game over ?', isGameOver());
     }
 
     function getPositionTo(anchor: Case, relative: Case): string {
@@ -119,8 +110,113 @@ export default defineComponent({
       if (pathGoal) {
         res.push(`-${pathGoal.color}`);
         res.push('-last');
+        if (isNeighbors(pathGoal.goal, pathGoal.cases[pathGoal.cases.length - 1]) && pathGoal.cases.length > 1) {
+          res.push('-complete');
+        }
       }
       return res;
+    }
+
+    function getAllClass(): void {
+      for (let i = 0; i < 9; i += 1) {
+        for (let j = 0; j < 9; j += 1) {
+          game.rows[i].cases[j].classes = getClass(game.rows[i].cases[j]);
+        }
+      }
+    }
+
+    function updateNeighborsClass(c: Case) {
+      getNeighbors(c).forEach((item) => {
+        // eslint-disable-next-line no-param-reassign
+        item.classes = getClass(item);
+      });
+    }
+
+    function handleSelect(c: Case): void {
+      const neighbors: Case [] = getNeighbors(c);
+      const casePath: Path | undefined = getCasePath(c);
+      if (casePath && casePath.cases[casePath.cases.length - 1] === c && casePath.cases[0] !== c) {
+        casePath.cases.splice(casePath.cases.length - 1, 1);
+        // eslint-disable-next-line no-param-reassign
+        c.classes = getClass(c);
+        updateNeighborsClass(c);
+      } else if (!casePath) {
+        const pathNeighbors: Case [] = neighbors.filter((n) => {
+          const path = getCasePath(n);
+          if (path) {
+            return path.cases[path.cases.length - 1] === n;
+          }
+          return false;
+        });
+        if (pathNeighbors.length > 0) {
+          const path: Path | undefined = getCasePath(pathNeighbors[0]);
+          if (path && c !== path.goal) {
+            path.cases.push(c);
+            // eslint-disable-next-line no-param-reassign
+            c.classes = getClass(c);
+            updateNeighborsClass(c);
+          }
+        }
+      }
+      if (isGameOver()) {
+        console.log('The game is over');
+        emit('endGame');
+      }
+    }
+
+    function getRandomCase(boardSize: number): Case {
+      const i = Math.floor(Math.random() * boardSize);
+      const j = Math.floor(Math.random() * boardSize);
+      return { x: i, y: j, classes: [] };
+    }
+
+    function generateRandomPath(gamePaths: Case [][], path: Case []): Case [] {
+      let c1: Case;
+      if (path.length === 0) {
+        c1 = getRandomCase(9);
+      } else {
+        c1 = path[path.length - 1];
+      }
+      let c2: Case | undefined = getNeighbors(c1).find((c) => gamePaths.findIndex((p) => p.includes(c)) === -1);
+      let it = 1;
+      // eslint-disable-next-line no-loop-func
+      while ((gamePaths.findIndex((p) => p.includes(c1)) !== -1 || c2 === undefined) && it < 81) {
+        c1 = getRandomCase(9);
+        c2 = getNeighbors(c1).find((c) => getCasePath(c) === undefined);
+        it += 1;
+      }
+      if (c2 === undefined) {
+        throw new Error('unable to generated game');
+      }
+      if (path.length < 10) {
+        path.push(c1);
+        path.push(c2);
+        try {
+          generateRandomPath(gamePaths, path);
+        } catch {
+          return path;
+        }
+      }
+      return path;
+    }
+
+    function generateGame(nb: number): void {
+      const generatedPaths: Case [][] = [];
+      const newPaths: Path [] = [];
+      for (let i = 0; i < nb; i += 1) {
+        generatedPaths.push(generateRandomPath(generatedPaths, []));
+      }
+      console.log(generatedPaths);
+      generatedPaths.forEach((path, index) => {
+        const first = path[0];
+        const last = path[path.length - 1];
+        newPaths.push({
+          color: Object.values(PathColor)[index % Object.keys(PathColor).length],
+          cases: [game.rows[last.x].cases[last.y]],
+          goal: game.rows[first.x].cases[first.y],
+        });
+      });
+      paths = newPaths;
     }
 
     onMounted(() => {
@@ -128,20 +224,13 @@ export default defineComponent({
         const row: Row = { cases: [] };
 
         for (let j = 0; j < 9; j += 1) {
-          row.cases.push({ x: i, y: j });
+          row.cases.push({ x: i, y: j, classes: [] });
         }
         game.rows.push(row);
       }
-      paths.push({
-        color: 'blue',
-        cases: [game.rows[0].cases[0]],
-        goal: game.rows[5].cases[5],
-      });
-      paths.push({
-        color: 'red',
-        cases: [game.rows[8].cases[8]],
-        goal: game.rows[6].cases[4],
-      });
+      generateGame(6);
+      getAllClass();
+      console.log(paths, game);
     });
 
     return {
