88b42afd4d9d8ef7011ae9a91d8ed1805c21e643
ClementineBiais <clementine.biais@live.fr>
2022-02-11 18:11:47 +0100

    finishing result-page

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -4,23 +4,23 @@
       <nav class="dt-w100 dt-h16">
         <div class="icon-close dt-floatR dt-grey" @click="$emit('close')"></div>
       </nav>
-      <div class="dt-flex-text--center">
+      <div class="dt-flex-center">
         <span class="icon-trophy dt-p16 dt-trophy--yellow dt-font-xxxl"></span>
         <h2 class="dt-inline-block">Félicitations !</h2>
       </div>
     </header>
-    <section class="dt-p16">
-      <div class="dt-results-titlecontainer">
-        <h4 class="island-name">{{jobData.islandName}}</h4>
+    <article class="dt-p16">
+      <section class="dt-results-titlecontainer">
+        <h4 class="dt-results-h4">{{jobData.islandName}}</h4>
         <h2 class="dt-results-h2">{{jobData.field}}</h2>
-      </div>
+      </section>
     <TextContainer class="dt-p16 dt-textcontainer--neutral">
-      <h3>{{jobData.field}}, kesako ?</h3>
+      <h4 class="dt-results-h4">{{jobData.field}}, kesako ?</h4>
       <p>{{jobData.results.summary}}</p>
     </TextContainer>
-    <br>
-    <div class="dt-results-sliderscontainer dt-p16">
-      <h4>Caractéristiques</h4>
+
+    <section class="dt-results-sliderscontainer dt-p16">
+      <h4 class="dt-results-h4">Caractéristiques</h4>
       <div class="dt-results-oneslidercontainer" v-for="caracteristic in jobData.results.caracteristics" :key="caracteristic.labelLeft">
         <span class="dt-slider-label">
           <div :class="`icon-${caracteristic.iconLeft}`"></div>
@@ -32,41 +32,50 @@
           <p>{{caracteristic.labelRight}}</p>
         </span>
       </div>
-    </div>
-
-    <h3>Focus sur un métier</h3>
-    <h2 class="dt-results-h2">{{jobData.results.specificJob.jobName}}</h2>
-    <TextContainer class="dt-textcontainer--outlined--blue">
-      <h3>Le travail au quotidien</h3>
-      <p>{{jobData.results.specificJob.generalInfos}}</p>
-      <TextContainer class="dt-textcontainer--blue dt-results--statistics-container">
-        <div class="dt-results--statistics-content">
-          <strong>{{jobData.results.specificJob.salary / 12}}</strong>
-          <br>
-          € brut / mois
-        </div>
-        <div class="dt-results--statistics-content" >
-          <strong>{{jobData.results.specificJob.studies}}</strong>
-        </div>
-      </TextContainer>
-      <h3>Quel impact ?</h3>
-      <p>Etre toi a de nombreux impacts positifs sur blabla... Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
-        illum, quaerat iure eius animi ratione alias omnis consequatur cum voluptas error eveniet,
-        ipsum possimus dignissimos facilis asperiores amet vel quae.</p>
-      <Button class="dt-button -blue" @click="$emit('close')">En savoir plus</Button>
-      <Button class="dt-button -blue -outlined" @click="$emit('close')">Voir d'autres métiers</Button>
-    </TextContainer>
-    <hr class="dt-results-separator">
-      <div>
-        <h3>Le témoignage d'une pro :</h3>
-        <iframe src="https://www.youtube.com/embed/O8ouH41S2fs" title="YouTube video player"
-          frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
-          gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
-
-    <hr class="dt-results-separator">
-    <Button class="dt-button -blue" @click="$emit('close')">Retour à la carte</Button>
     </section>
+
+    <section>
+      <h3 class="dt-results-h3">Focus sur un métier</h3>
+      <h2 class="dt-results-h2">{{jobData.results.specificJob.jobName}}</h2>
+      <TextContainer class="dt-textcontainer--outlined--blue">
+        <h4 class="dt-results-h4">Le travail au quotidien</h4>
+        <p>{{jobData.results.specificJob.generalInfos}}</p>
+        <TextContainer class="dt-textcontainer--blue dt-results--statistics-container">
+          <div class="dt-results--statistics-content">
+            <strong>{{jobData.results.specificJob.salary / 12}}</strong>
+            <br>
+            € brut / mois
+          </div>
+          <div class="dt-results--statistics-content">
+            <strong>{{jobData.results.specificJob.studies}}</strong>
+          </div>
+        </TextContainer>
+        <h4 class="dt-results-h4">Quel impact ?</h4>
+        <p>Etre toi a de nombreux impacts positifs sur blabla... Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+          illum, quaerat iure eius animi ratione alias omnis consequatur cum voluptas error eveniet,
+          ipsum possimus dignissimos facilis asperiores amet vel quae.</p>
+        <a v-if="jobData.results.specificJob.moreInfo" :href="jobData.results.specificJob.moreInfo">
+          <Button class="dt-button -blue -detail">En savoir plus</Button>
+        </a>
+        <a v-if="jobData.results.specificJob.moreJobs" :href="jobData.results.specificJob.moreJobs">
+          <Button class="dt-button -blue -outlined -detail">Voir d'autres métiers</Button>
+        </a>
+      </TextContainer>
+    </section>
+
+    <section v-if="jobData.results.specificJob.videoUrl" class="dt-flex-align-text--center">
+      <hr class="dt-results-separator">
+        <h4 class="dt-results-h4">Le témoignage d'une pro :</h4>
+        <iframe :src="jobData.results.specificJob.videoUrl" title="YouTube video player"
+          frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+          gyroscope; picture-in-picture" allowfullscreen class="dt-results-video"></iframe>
+      <hr class="dt-results-separator">
+    </section>
+
+    <footer>
+      <Button class="dt-button -blue" @click="$emit('close')">Retour à la carte</Button>
+    </footer>
+    </article>
   </div>
 </template>
 
