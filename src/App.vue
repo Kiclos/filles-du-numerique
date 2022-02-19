2169811d47f6c5f15d93eea181e8367ca4dbf068
Thomas Gaubet <thomas.gaubert49@gmail.com>
2022-02-19 16:28:35 +0100

    hotfix: screen size

--- src/App.vue
+++ src/App.vue
@@ -98,7 +98,17 @@ export default defineComponent({
       }
     }
 
+    function getJavascriptViewPort(): void {
+      // We listen to the resize event
+      window.addEventListener('resize', () => {
+        // We execute the same script as before
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+      });
+    }
+
     onMounted(() => {
+      getJavascriptViewPort();
       const gameStatus = localStorage.getItem('gameStatus');
       if (gameStatus !== null) {
         gameStore.setGameStatus(JSON.parse(gameStatus));
