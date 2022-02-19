d49502b8a95bb848c905ebe15845350e322d5a23
Thomas Gaubet <thomas.gaubert49@gmail.com>
2022-02-19 18:35:37 +0100

    hotfix: install button rmeoved for iphones

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,7 +3,7 @@
     <img class="dt-img-w75" src="@/assets/departile.svg" alt="">
     <h1 class="dt-h1">DigitTales</h1>
     <button class="dt-button" @click="$emit('play')">Jouer</button>
-    <button class="dt-button -outlined" @click="install">Installer l'application</button>
+    <button class="dt-button -outlined" @click="install" v-if="!isIphone">Installer l'application</button>
     <button class="dt-button -text">Crédits</button>
   </div>
 </template>
@@ -15,6 +15,18 @@ export default defineComponent({
   name: 'welcome',
   data() {
     return { deferredPrompt: null as any };
+  },
+  computed: {
+    isIphone() {
+      return [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod',
+      ].includes(navigator.platform);
+    },
   },
   created() {
     window.addEventListener('beforeinstallprompt', e => {
