6e8d8ff0ba341b161392a3e3953fc9fa2045edde
Thomas Gaubet <thomas.gaubert49@gmail.com>
2022-02-25 08:45:29 +0100

    feat: credit view

---
+++ src/views/CreditView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dt-credit">
+    <div class="dt-credit__content">
+      <h1 class="dt-credit__title">Crédits</h1>
+      <h2 class="dt-credit__subtitle">
+        <span class="dt-credit__subtitle__label">Développeurs</span>
+        Clémentine Biais <br>
+        Thomas Gaubert
+      </h2>
+      <h2 class="dt-credit__subtitle">
+        <span class="dt-credit__subtitle__label">Partenaires</span>
+        FACET <br>
+        Femmes du numérique
+      </h2>
+
+    </div>
+    <button class="dt-button" @click="handleClose()">Retour au menu</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CreditView',
+  emits: ['close'],
+  events: { close: () => null },
+  setup(_, { emit }) {
+    function handleClose(): void {
+      emit('close');
+    }
+
+    return { handleClose };
+  },
+});
+</script>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,20 +1,28 @@
 <template>
+  <Transition name="zoom-fade">
+    <CreditView @close="creditIsOpened = false" v-if="creditIsOpened"/>
+  </Transition>
   <div class="dt-home-screen">
     <img class="dt-img-w75" src="@/assets/departile.svg" alt="">
     <h1 class="dt-h1">DigitTales</h1>
     <button class="dt-button" @click="$emit('play')">Jouer</button>
     <button class="dt-button -outlined" @click="install" v-if="!isIphone">Installer l'application</button>
-    <button class="dt-button -text">Crédits</button>
+    <button class="dt-button -text" @click="creditIsOpened = true">Crédits</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import CreditView from '@/views/CreditView.vue';
 
 export default defineComponent({
   name: 'welcome',
+  components: { CreditView },
   data() {
-    return { deferredPrompt: null as any };
+    return {
+      deferredPrompt: null as any,
+      creditIsOpened: false,
+    };
   },
   computed: {
     isIphone() {
