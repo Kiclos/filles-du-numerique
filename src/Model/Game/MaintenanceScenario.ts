12d1377737412105bfa250801437b4f0396b44b6
Lois <lois.gigaud85@gmail.com>
2023-01-11 11:43:18 +0100

    Fix apparition des messages de choix + déplacement du scénario

---
+++ src/Model/Game/MaintenanceScenario.ts
@@ -0,0 +1,159 @@
+import type { EtapeHistoireType } from '@/components/Games/MaintenanceGame.vue'
+
+export const histoire: EtapeHistoireType[] = [
+  {
+    messagesList: [
+      {
+        content: 'Bonjour, j\'ai besoin d\'aide avec mon ordinateur !',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Sinon une catastrophe va se produire !',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Calmez vous et expliquez moi votre problème',
+        isImage: false,
+        isMine: true,
+      },
+      {
+        content: 'Mon ordinateur contrôle et empêche les éruptions du volcan de l\'île',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Cependent depuis ce matin il ne fonctione plus...',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Si je n\'arrive pas a le faire marcher, le volcan va entrer en éruption et des débris risquent de détruie l\'île !',
+        isImage: false,
+        isMine: false,
+      },
+    ],
+    choixList: {
+      content: ['Avez vous essayer de le redémarrer ?', 'Avez vous essayer de taper dessus ?'],
+      goodChoice: 0,
+    },
+  },
+  {
+    messagesList: [
+      {
+        content: 'Avez vous essayer de le redémarrer ?',
+        isImage: false,
+        isMine: true,
+      },
+      {
+        content: 'Oui j\'ai déjà essayé ...',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Voici une photo de mon ordinateur',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'meteorite.png',
+        isImage: true,
+        isMine: false,
+      },
+    ],
+    choixList: {
+      content: ['Votre cable est débranché', 'Essayez de mettre votre ordinateur dans du riz'],
+      goodChoice: 0,
+    },
+  },
+  {
+    messagesList: [
+      {
+        content: 'Votre cable est débranché',
+        isImage: false,
+        isMine: true,
+      },
+      {
+        content: 'Oui effectivement ...',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'J\'arrive à l\'allumer maintenant !',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Je n\'arrive pas à me connecter à internet maintenant !',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'meteorite.png',
+        isImage: true,
+        isMine: false,
+      },
+    ],
+    choixList: {
+      content: ['Essayez d\'éteindre votre écran', 'Vous n\'êtes pas connecté à la wifi, vérifiez le mot de passe'],
+      goodChoice: 1,
+    },
+  },
+  {
+    messagesList: [
+      {
+        content: 'Vous n\'êtes pas connecté à la wifi, vérifiez le mot de passe',
+        isImage: false,
+        isMine: true,
+      },
+      {
+        content: 'Ah oui c\'est vrai j\'avais changé le mot de passe',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'J\'ai un nouveau problème maintenant',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Des pubs m\'empêchent d\'utiliser l\'ordinateur !!',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'meteorite.png',
+        isImage: true,
+        isMine: false,
+      },
+    ],
+    choixList: {
+      content: ['Installez un antivirus', 'Mettez une etiquette stop pub sur votre ordinateur', 'Achetez les produits des pubs'],
+      goodChoice: 0,
+    },
+  },
+  {
+    messagesList: [
+      {
+        content: 'Installez un antivirus',
+        isImage: false,
+        isMine: true,
+      },
+      {
+        content: 'Merci beaucoup, mon ordinateur fonctionne maintenant',
+        isImage: false,
+        isMine: false,
+      },
+      {
+        content: 'Vous avez sauver mon île, merci beaucoup !',
+        isImage: false,
+        isMine: false,
+      },
+    ],
+    choixList: {
+      content: ['De rien, bonne journée'],
+      goodChoice: 0,
+    },
+  },
+]
--- src/components/Games/MaintenanceGame.vue
+++ src/components/Games/MaintenanceGame.vue
@@ -7,9 +7,9 @@ import PauseMenu from '@/components/Games/GamesUI/PauseMenu/PauseMenu.vue'
 import Meteorite from '@/components/Games/MaintenanceGame/Meteorite.vue'
 import Messages, { MessageType } from '@/components/Games/MaintenanceGame/Messages.vue'
 import Choix from '@/components/Games/MaintenanceGame/Choix.vue'
-import { connect } from 'http2'
+import { histoire } from '@/Model/Game/MaintenanceScenario'
 
-export interface EtapeHistoireType {
+export type EtapeHistoireType = {
   messagesList: MessageType[],
   choixList: ChoixType
 }
@@ -34,164 +34,10 @@ export default defineComponent({
     function handleCountdown(): number {
       return countdown
     }
-    const histoire: EtapeHistoireType[] = [
-      {
-        messagesList: [
-          {
-            content: 'Bonjour, j\'ai besoin d\'aide avec mon ordinateur !',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Sinon une catastrophe va se produire !',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Calmez vous et expliquez moi votre problème',
-            isImage: false,
-            isMine: true,
-          },
-          {
-            content: 'Mon ordinateur contrôle et empêche les éruptions du volcan de l\'île',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Cependent depuis ce matin il ne fonctione plus...',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Si je n\'arrive pas a le faire marcher, le volcan va entrer en éruption et des débris risquent de détruie l\'île !',
-            isImage: false,
-            isMine: false,
-          },
-        ],
-        choixList: {
-          content: ['Avez vous essayer de le redémarrer ?', 'Avez vous essayer de taper dessus ?'],
-          goodChoice: 0
-        },
-      },
-      {
-        messagesList: [
-          {
-            content: 'Avez vous essayer de le redémarrer ?',
-            isImage: false,
-            isMine: true,
-          },
-          {
-            content: 'Oui j\'ai déjà essayé ...',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Voici une photo de mon ordinateur',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'meteorite.png',
-            isImage: true,
-            isMine: false,
-          },
-        ],
-        choixList: {
-          content: ['Votre cable est débranché', 'Essayez de mettre votre ordinateur dans du riz'],
-          goodChoice: 0
-        }
-      },
-      {
-        messagesList: [
-          {
-            content: 'Votre cable est débranché',
-            isImage: false,
-            isMine: true,
-          },
-          {
-            content: 'Oui effectivement ...',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'J\'arrive à l\'allumer maintenant !',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Je n\'arrive pas à me connecter à internet maintenant !',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'meteorite.png',
-            isImage: true,
-            isMine: false,
-          },
-        ],
-        choixList: {
-          content: ['Essayez d\'éteindre votre écran', 'Vous n\'êtes pas connecté à la wifi, vérifiez le mot de passe'],
-          goodChoice: 1
-        }
-      },
-      {
-        messagesList: [
-          {
-            content: 'Vous n\'êtes pas connecté à la wifi, vérifiez le mot de passe',
-            isImage: false,
-            isMine: true,
-          },
-          {
-            content: 'Ah oui c\'est vrai j\'avais changé le mot de passe',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'J\'ai un nouveau problème maintenant',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Des pubs m\'empêchent d\'utiliser l\'ordinateur !!',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'meteorite.png',
-            isImage: true,
-            isMine: false,
-          },
-        ],
-        choixList: {
-          content: ['Installez un antivirus', 'Mettez une etiquette stop pub sur votre ordinateur', 'Achetez les produits des pubs'],
-          goodChoice: 0
-        }
-      },
-      {
-        messagesList: [
-          {
-            content: 'Installez un antivirus',
-            isImage: false,
-            isMine: true,
-          },
-          {
-            content: 'Merci beaucoup, mon ordinateur fonctionne maintenant',
-            isImage: false,
-            isMine: false,
-          },
-          {
-            content: 'Vous avez sauver mon île, merci beaucoup !',
-            isImage: false,
-            isMine: false,
-          },
-        ],
-        choixList: {
-          content: ['De rien, bonne journée'],
-          goodChoice: 0
-        }
-      },
-    ]
+    // const histoire: EtapeHistoireType[] = 
     const etapeIndex = ref(0)
+    const choixList = ref<ChoixType | null>()
+    const choixAvailable = ref(false)
     const messageList = ref<MessageType[]>([])
     const cpt = ref(0)
     ajouterNouveauMessages()
@@ -202,6 +48,8 @@ export default defineComponent({
     const handleChoix = (choixNumber: number): void => {
       if (histoire[etapeIndex.value]?.choixList.goodChoice === choixNumber) {
         if (etapeIndex.value + 1 < histoire.length) {
+          choixList.value = null
+          choixAvailable.value = false
           etapeIndex.value += 1
           ajouterNouveauMessages()
         }
@@ -225,6 +73,8 @@ export default defineComponent({
           messagesBox.scrollTop = messagesBox.scrollHeight
         }
         else {
+          choixList.value = histoire[etapeIndex.value].choixList
+          choixAvailable.value = true
           cpt.value = 0
           clearInterval(interval)
         }
@@ -249,6 +99,8 @@ export default defineComponent({
       messageList,
       modaleOpen,
       modaleLoseGame,
+      choixList,
+      choixAvailable,
       toggleModaleBadAnswer,
       handleCountdown,
       handleSkipGame,
@@ -293,7 +145,7 @@ export default defineComponent({
       <Messages :messages="messageList" />
     </template>
     <template #footer>
-      <Choix :choix-list="histoire[etapeIndex]?.choixList" @handle-choix="handleChoix" />
+      <Choix :choix-list="choixList" :choix-available="choixAvailable" @handle-choix="handleChoix" />
     </template>
   </Game>
 </template>
